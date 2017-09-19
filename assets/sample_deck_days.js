716d455d5bbf0628803233f7a05f669799ac2036
Alex Read <alexread730@gmail.com>
2017-09-18 22:29:03 -0600

    added production db connection

--- assets/sample_deck_days.js
+++ assets/sample_deck_days.js
@@ -1,7 +1,49 @@
 const deck_days = [
   {
     deck_id: 1,
+    day_id: 1,
+    startTime: 9,
+    endTime: 23,
+    interval: 30
+  },
+  {
+    deck_id: 2,
     day_id: 2,
+    startTime: 9,
+    endTime: 23,
+    interval: 30
+  },
+  {
+    deck_id: 1,
+    day_id: 3,
+    startTime: 9,
+    endTime: 23,
+    interval: 30
+  },
+  {
+    deck_id: 2,
+    day_id: 4,
+    startTime: 9,
+    endTime: 23,
+    interval: 30
+  },
+  {
+    deck_id: 1,
+    day_id: 5,
+    startTime: 9,
+    endTime: 23,
+    interval: 30
+  },
+  {
+    deck_id: 2,
+    day_id: 6,
+    startTime: 9,
+    endTime: 23,
+    interval: 30
+  },
+  {
+    deck_id: 1,
+    day_id: 7,
     startTime: 9,
     endTime: 23,
     interval: 30
@@ -15,17 +57,31 @@ const deck_days = [
   },
   {
     deck_id: 3,
+    day_id: 3,
+    startTime: 9,
+    endTime: 23,
+    interval: 10
+  },
+  {
+    deck_id: 3,
     day_id: 4,
     startTime: 9,
-    endTime: 17,
+    endTime: 23,
     interval: 10
   },
   {
     deck_id: 4,
     day_id: 1,
     startTime: 9,
-    endTime: 17,
-    interval: 60
+    endTime: 23,
+    interval: 20
+  },
+  {
+    deck_id: 4,
+    day_id: 6,
+    startTime: 9,
+    endTime: 23,
+    interval: 20
   }
 ]
 
--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/incheck'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
