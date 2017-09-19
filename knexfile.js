519d20cbf23de4b09ec43a246edd4666eb684c34
Alex Read <alexread730@gmail.com>
2017-09-18 22:40:18 -0600

    hidden url

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://weprozpzfjsiaa:3ef151912f11e2062b6860ba3f836b49e18dde30fba5fe6e2f384e1787d2d86a@ec2-184-72-248-8.compute-1.amazonaws.com:5432/d3mlmbg7pebbi3'
+    connection: process.env.DATABASE_URL
   }
 
 };
