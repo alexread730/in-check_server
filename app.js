b7780fb61c7468a0146d45621655c043a78695d5
Alex Read <alexread730@gmail.com>
2017-09-25 16:06:08 -0600

    working auth kind of

--- app.js
+++ app.js
@@ -46,9 +46,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+console.log(err.stack);
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message
+  });
 });
 
 module.exports = app;
